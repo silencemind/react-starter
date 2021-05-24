c7584e32965ee836592019eea616337cc44b6ebb
silencemind <silencemind55@gmail.com>
2021-05-24 22:56:42 +0500

    .

--- app1/src/index.js
+++ app1/src/index.js
@@ -1,41 +1,47 @@
-import React ,{Component}from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-const app = () => {
-  const course  = "half stack development"
-  const part1 = 'fundamental of react '
-  const exercise1 = 10
-  const part2 = 'using props to pass data'
-  const exercise2 = '7'
-  const part3 = 'State of a component'
-  const exercise3 = 14
-  return (
-    <div>
-    <h1>{course}</h1>
-    <p>
-      {part1} {exercise1}
-    </p>
-    <p>
-      {part2} {exercise2}
-    </p>
-    <p>
-      {part3} {exercise3}
-    </p>
-    <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
-  </div>
 
-  )
-}
-class App extends Component{ 
-  render(){
+class App extends Component {
+  render() {
     return (
-  <div className="App">
-      <h1> Hello, React</h1>
-    </div>
+      <div className="abid">
+        <h1>Hello, Abid!</h1>
+      </div>
     )
   }
 }
+
+// const App = () => {
+// class App extends Component {
+//   render(){
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+// }
+
 ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<app />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
